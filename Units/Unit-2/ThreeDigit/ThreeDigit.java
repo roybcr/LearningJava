d8b74c9df9e9482e7a37e875232a79f4189caa46
Roy <68123426+roybcr@users.noreply.github.com>
2021-10-19 12:11:37 +0300

    3-digit printer

--- Units/Unit-2/ThreeDigit/ThreeDigit.java
+++ Units/Unit-2/ThreeDigit/ThreeDigit.java
@@ -12,22 +12,15 @@ public class ThreeDigit {
     public static void main(String[] args) {
 
         int num;
-
-        // Initializing a new Scanner instance
         Scanner scan = new Scanner(System.in);
+        
         System.out.println("Given a 3 digit number,This program prints each digit starts from the units, in a seperate line.");
         System.out.println("Please enter a three-digit number: ");
         num = scan.nextInt();
 
-        int units = num % 10;
-        num /= 10;
-        int tens = num % 10;
-        num /= 10;
-        int hundreds = num;
-
-        System.out.println(units);
-        System.out.println(tens);
-        System.out.println(hundreds);
+        System.out.println(num % 10); // Units
+        System.out.println(num / 10 % 10); // Tens
+        System.out.println(num / 100); // Hundreds
 
         scan.close();
 
