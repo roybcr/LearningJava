1b577d7ba53175357a890a5b19e3af7e7cb3de8d
Roy <roybcx@pm.me>
2021-11-26 23:00:51 +0200

    setup tests for RectA

--- Code/MMM12/Tester.java
+++ Code/MMM12/Tester.java
@@ -7,7 +7,6 @@
 
 public class Tester {
 
-    
     static int TEST_NO = 0;
 
     public static void main(String[] args) {
@@ -29,6 +28,52 @@ public class Tester {
         runPairPointTests(p1, p2, 4, 8, 0, 0);
         runPairPointTests(p2, p3, 0, 0, 10, 20);
         runPairPointTests(p3, p1, 10, 20, 4, 8);
+
+        /** Single RectangleA Tests */
+        Point rA_SW = new Point(-10, -2);
+        Point rA_NE = new Point(2, 4);
+        int rA_width = 12;
+        int rA_height = 6;
+        double rA_Diagonal = 13.416407864998739;
+        int rA_perimeter = 36;
+        int rA_area = 72;
+
+        Point rB_SW = new Point(-10, -4);
+        Point rB_NE = new Point(-2, 8);
+        int rB_width = 8;
+        int rB_height = 4;
+        double rB_Diagonal = 9.848857801796104;
+        int rB_perimeter = 24;
+        int rB_area = 32;
+
+        Point rC_SW = new Point(-6, 6);
+        Point rC_NE = new Point(2, 10);
+        int rC_width = 8;
+        int rC_height = 4;
+        double rC_Diagonal = 8.94427190999916;
+        int rC_perimeter = 24;
+        int rC_area = 32;
+
+        Point rD_SW = new Point(0, 0);
+        Point rD_NE = new Point(6, 8);
+        int rD_width = 6;
+        int rD_height = 8;
+        double rD_Diagonal = 10.0;
+        int rD_perimeter = 28;
+        int rD_area = 48;
+
+        RectangleA rA = new RectangleA(rA_SW, 12, 6);
+        RectangleA rB = new RectangleA(rB_SW, 8, 4);
+        RectangleA rC = new RectangleA(rC_SW, 8, 4);
+        RectangleA rD = new RectangleA(8, 6);
+
+        System.out.println(
+                "========================================= RectangleA ============================================");
+
+        runSingleRectTests(rA, rA_width, rA_height, rA_SW, rA_NE, rA_Diagonal, rA_perimeter, rA_area);
+        runSingleRectTests(rB, rB_width, rB_height, rB_SW, rB_NE, rB_Diagonal, rB_perimeter, rB_area);
+        runSingleRectTests(rC, rC_width, rC_height, rC_SW, rC_NE, rC_Diagonal, rC_perimeter, rC_area);
+        runSingleRectTests(rD, rD_width, rD_height, rD_SW, rD_NE, rD_Diagonal, rD_perimeter, rD_area);
     }
 
     public static void testNo(String id) {
@@ -178,5 +223,39 @@ public class Tester {
 
         prnt(p2.distance(p1));
         exp(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+    }
+
+    public static void runSingleRectTests(RectangleA r, int w, int h, Point sw, Point ne, double diagonal,
+            int perimeter,
+            int area) {
+        prnt(r.getWidth());
+        exp(w);
+
+        prnt(r.getHeight());
+        exp(h);
+
+        prnt(r.getPointSW());
+        exp(sw);
+
+        prnt(r.getPerimeter());
+        exp(perimeter);
+
+        prnt(r.getArea());
+        exp(area);
+
+        prnt(r.getDiagonalLength());
+        exp(diagonal);
+
+        prnt(r.getPointNE());
+        exp(ne);
+
+        r.changeSides();
+
+        prnt(r.getPointSW());
+        exp(ne);
+
+        prnt(r.getPointNE());
+        exp(sw);
+
     }
 }
