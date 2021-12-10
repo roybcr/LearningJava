ab6ac7ffde27f6b0089e53af7fc17a056911e99f
Roy <roybcx@pm.me>
2021-12-10 14:00:49 +0200

    unit6

--- Units/Unit-6/Matrix/Matrix/OnePair.java
+++ Units/Unit-6/Matrix/Matrix/OnePair.java
@@ -1,21 +1,21 @@
 /**
- * Given a matrix, return true if it has at one pair of numbers
- * which sums to 1.
+ * Given a matrix, return true if it has at least one pair of numbers
+ * that sums to 1.
  * 
  * @Author (Roy Bachar)
  * @Version(09/12/21)
  */
- 
+
 public class OnePair {
-    
+
     public static boolean pair1(int[][] a) {
-        for(int i = 0; i < a.length; i++) 
-            for(int j = 0; j < a[0].length; j++)
-                for(int m = 0; m < a.length; m++)
-                    for(int k = 0; k < a[0].length; k++)
-                        if(a[m][k] + a[i][j] == 1)
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[0].length; j++)
+                for (int m = 0; m < a.length; m++)
+                    for (int k = 0; k < a[0].length; k++)
+                        if (a[m][k] + a[i][j] == 1)
                             return true;
-        
+
         return false;
     }
 
